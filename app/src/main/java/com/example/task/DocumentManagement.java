d1ac682c9863d7d11de420dc82865479fd3a2245
hafizhamza660 <hafizhamza660@gmail.com>
2021-09-06 19:02:41 +0500

    6-9-2021

--- app/src/main/java/com/example/task/DocumentManagement.java
+++ app/src/main/java/com/example/task/DocumentManagement.java
@@ -1,17 +1,80 @@
 package com.example.task;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class DocumentManagement extends AppCompatActivity {
+
+    CardView id_card_btn;
+    ImageView id_card_img,id_card_demo;
+
+    private int PICK_IMAGE_REQUEST = 1;
+
+    private Bitmap bitmap;
+
+    private Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_document_management);
+
+        id_card_btn= findViewById(R.id.id_card_btn);
+        id_card_img= findViewById(R.id.id_card_img);
+        id_card_demo= findViewById(R.id.id_card_demo);
+
+        id_card_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFileChooser();
+            }
+        });
+
+    }
+
+    public String getStringImage(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            filePath = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                id_card_demo.setVisibility(View.GONE);
+                id_card_img.setVisibility(View.VISIBLE);
+                id_card_img.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void back_button(View view) {
